c846012acb1eca046dbc9284d5f0831e1d2f0868
Kieran <mr.kieran.mcbride@googlemail.com>
2021-02-10 12:13:28 +0000

    stying changes

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="contact">
-    <img src="@/assets/contact.png" />
+    <div class="container">
+      <div class="cols-12" id="header-banner">
+     </div>
+    </div>
   </div>
 </template>
 
@@ -12,3 +15,10 @@ export default {
   components: {}
 };
 </script>
+
+<style scoped>
+
+.container{padding: 0;}
+#header-banner{min-height: 30vh; background-image: url('../assets/contact-banner.jpg'); background-position: center; background-size: cover; font-size: 3.75rem; display: flex; align-items: center; justify-content: center; color: #2c3e50; font-family: 'Abril Fatface', cursive; opacity: 0.5;}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
       <b-col cols="12" md="4">
         <b-img src="@/assets/bio-img.png"></b-img>
       </b-col>
-      <b-col id="bio" cols="12" md="8">
+      <b-col id="bio" cols="12" sm="12" md="8">
         <h1 class="title">Hi, I'm Kieran</h1>
         <p class="bio-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis est incidunt ad quia dolore corporis at? Aut laborum, omnis laudantium inventore possimus numquam soluta recusandae maxime tempore dolorum? Possimus, blanditiis?</p>
 
@@ -26,9 +26,17 @@ export default {
 </script>
 
 <style scoped>
+
 .placeholder{padding: 0; text-align: left;}
 img {display: block; margin: 0 auto;}
-#bio {max-width: 60%;padding:  2% 10%;}
+#bio {padding:  2% 10%;}
 .title, .quote {font-family: 'Abril Fatface', cursive; font-size: 4rem;}
 .quote {color:#dbdbdb; padding-top: 8%;}
+
+@media only screen and (max-width: 768px) {
+  
+  .placeholder{padding: 0; text-align: center;}
+
+}
+
 </style>
