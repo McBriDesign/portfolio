ad3acee51a3f529b375ea373a8f14593f05ac8fe
Kieran <mr.kieran.mcbride@googlemail.com>
2021-03-10 14:33:41 +0000

    ...

--- vue.config.js
+++ vue.config.js
@@ -10,9 +10,9 @@ module.exports = {
           // Absolute path to compiled SPA
           path.resolve(__dirname, "dist"),
           // List of routes to prerender
-          ["/contact"],
+          ["/"],
           {
-            // options
+            '#/contact'
           }
         )
       ]
